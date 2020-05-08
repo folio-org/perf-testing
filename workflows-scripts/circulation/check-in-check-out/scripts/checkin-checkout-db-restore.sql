6b0d4245a2153cceb7ddbff2b0b9d6a3d5970d13
mtraneis <mtran@ebsco.com>
2020-05-08 11:46:28 -0400

    added comments explaining what this script does and how to execute it

--- workflows-scripts/circulation/check-in-check-out/scripts/checkin-checkout-db-restore.sql
+++ workflows-scripts/circulation/check-in-check-out/scripts/checkin-checkout-db-restore.sql
@@ -1,3 +1,12 @@
+/*
+To use this script, enter the command:
+$ psql -f checkin-checkout-db-restore.sql -a --echo-all
+Also supply the values for -h, -d, -U if applicable
+
+This script will remove all the loans and requests, and their related data from the tables. It is expected that these loans and requests will be repopulated by another script.
+Lastly the script update inventory Items' status to Available (because their status got changed to Checked out or In transit during the test run.)
+*/
+
 TRUNCATE TABLE fs09000000_mod_circulation_storage.loan;
 TRUNCATE TABLE fs09000000_mod_circulation_storage.audit_loan;
 TRUNCATE TABLE fs09000000_mod_circulation_storage.request;
