a77efe34c9e0853df619e3ca5f0ce73020cc21b8
Stanislav Miroshnichenko <celt@root.in.ua>
2020-10-29 11:18:24 +0200

    Perf-114 (#27)
    * Terraform stack for Carrier.IO created
    
    * Changed usage of AWS region
    
    * Fixed terraform subnets
    
    * Variables example added
    
    * Rebase from master

---
+++ carrier-io/terraform/README.md
@@ -0,0 +1,9 @@
+Define AWS profile
+```
+export AWS_PROFILE=<profile name>
+```
+Run terraform
+```
+$ terraform init
+$ terraform apply
+```
---
+++ carrier-io/terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region used for all resources"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type used for all EC2 instances"
+  default     = "m5.2xlarge"
+}
+
+variable "use_default_vpc" {
+  type        = bool
+  default     = true
+  description = "Should the default VPC for the region selected be used for Carrier.IO"
+}
+
+variable "vpc_id" {
+  type        = string
+  default     = null
+  description = "If use_default_vpc is false, the vpc id that Carrier.IO should use"
+}
+
+variable "ingressCIDRblock" {
+    type = list
+    default = [ "0.0.0.0/0" ]
+}
+
+variable "creds_output_path" {
+  description = "Where to save the id_rsa config file. Should end in a forward slash `/` ."
+  type        = string
+  default     = "./"
+}
+
+variable "carrier_master_password" {
+  type        = string
+  description = "Password for Keycloak, Redis, Minio, Grafana, Postgres"
+}
