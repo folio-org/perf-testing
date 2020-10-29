4d7d2e3e5e4c6828a5fcd59a8709357db0c28381
Stanislav Miroshnichenko <celt@root.in.ua>
2020-10-29 10:58:14 +0200

    Rebase from master

--- carrier-io/shared.groovy
+++ carrier-io/shared.groovy
@@ -38,49 +38,30 @@ def deleteStack(ctx){
 
 def executePerformanceTest(ctx, excludeTestsList, sendReports){
     
-    final files = findFiles(glob: 'workflows-scripts/**/*.jmx', excludes: excludeTestsList)
-    for (def i=0; i<files.length; i++) {
-        
-        def testName        = "${files[i].name.minus('.jmx')}"
-        def pathAndName     = "${files[i].path.minus('.jmx')}"
-        def parentFolder    = "${files[i].path.minus(files[i].name)}"
+        def testName        = ctx.testName
         def artifact        = "${testName}.zip"
-        def propertiesFile  = "${parentFolder}/test.properties"
-
-        // Read properties
-        def props = readProperties defaults: ctx, file: propertiesFile
-        def usersCount = Math.round((props.users.toInteger() / props.loadGeneratorsCount.toInteger()).floatValue())
+        def usersCount = Math.round((ctx.users.toInteger() / ctx.loadGeneratorsCount.toInteger()).floatValue())
 
         withCredentials([string(credentialsId: 'perf_carrier_io_token_u51', variable: 'carrierToken')]) {
-            if (props.uploadArtifact) {
-                withCredentials([file(credentialsId: 'perf_tests_credentials', variable: 'FILE')]) {
-                    sh(script: "mv ${FILE} ${parentFolder}/jmeter-supported-data")
-                    zip zipFile: "${artifact}", archive: false, dir: "${parentFolder}"
-                    //httpRequest httpMode: 'POST', uploadFile: "${pathAndName}.zip", customHeaders: [[name: 'Authorization', value: 'bearer ${carrierToken}']], url: "http://${reportingInstanceUrl}/api/v1/artifacts/${projectId}/${bucket}/${testName}.zip"
-                    sh "curl -sSL -X POST -D - http://${props.reportingInstanceUrl}/api/v1/artifacts/${props.projectId}/${props.bucket}/${artifact} \
-                        -H 'Authorization: bearer ${carrierToken}' \
-                        -F 'file=@${artifact}'"
-                }
-            }
 
             withCredentials([string(credentialsId: 'perf_redis_password_u51', variable: 'redisPassword')]) {
                 sh "docker pull getcarrier/control_tower:latest && docker run -t --rm \
-                    -e REDIS_HOST=${props.reportingInstanceUrl} \
+                    -e REDIS_HOST=${ctx.reportingInstanceUrl} \
                     -e REDIS_PASSWORD=${redisPassword} \
-                    -e loki_host=http://${props.reportingInstanceUrl} \
+                    -e loki_host=http://${ctx.reportingInstanceUrl} \
                     -e loki_port=3100 \
-                    -e build_id=build_${props.envType}_${BUILD_ID} \
-                    -e galloper_url=http://${props.reportingInstanceUrl} \
+                    -e build_id=build_${ctx.envType}_${BUILD_ID} \
+                    -e galloper_url=http://${ctx.reportingInstanceUrl} \
                     -e token=${carrierToken} \
-                    -e bucket=${props.bucket} \
-                    -e project_id=${props.projectId} \
-                    -e JVM_ARGS='-Xmx${props.lgMemory}g' \
-                    -e DURATION=${props.duration} \
+                    -e bucket=${ctx.bucket} \
+                    -e project_id=${ctx.projectId} \
+                    -e JVM_ARGS='-Xmx${ctx.lgMemory}g' \
+                    -e DURATION=${ctx.duration} \
                     -e artifact=${artifact} \
                     getcarrier/control_tower:latest \
                         -c getcarrier/perfmeter:latest \
-                        -e '{\"cmd\": \"-n -t /mnt/jmeter/${testName}.jmx -Jtest_name=${testName} -JDISTRIBUTION=${props.distribution} -Jtenant=${props.tenant} -Jtest.type=${props.testType} -Jenv.type=${props.envType} -JVUSERS=${usersCount} -JHOSTNAME=${props.targetUrl} -JRAMP_UP=${props.rampUp} -JDURATION=${props.duration} -Jinflux.host=${props.reportingInstanceUrl} \"}' \
-                        -r 1 -t perfmeter -q ${props.loadGeneratorsCount} -n performance_test_job"
+                        -e '{\"cmd\": \"-n -t /mnt/jmeter/${testName}.jmx -Jtest_name=${testName} -JDISTRIBUTION=${ctx.distribution} -Jtenant=${ctx.tenant} -Jtest.type=${ctx.testType} -Jenv.type=${ctx.envType} -JVUSERS=${usersCount} -JHOSTNAME=${ctx.targetUrl} -JRAMP_UP=${ctx.rampUp} -JDURATION=${ctx.duration} -Jinflux.host=${ctx.influxDbUrl} \"}' \
+                        -r 1 -t perfmeter -q ${ctx.loadGeneratorsCount} -n performance_test_job"
             }
         }
         
@@ -89,8 +70,6 @@ def executePerformanceTest(ctx, excludeTestsList, sendReports){
             sendNotification(props, testName, usersCount)
         }
 
-        //break; // remove to run all tests
-    }
 }
 
 def executePerformanceTests(ctx, excludeTestsList, sendReports){
@@ -136,7 +115,7 @@ def executePerformanceTests(ctx, excludeTestsList, sendReports){
                     -e artifact=${artifact} \
                     getcarrier/control_tower:latest \
                         -c getcarrier/perfmeter:latest \
-                        -e '{\"cmd\": \"-n -t /mnt/jmeter/${testName}.jmx -Jtest_name=${testName} -JDISTRIBUTION=${props.distribution} -Jtenant=${props.tenant} -Jtest.type=${props.testType} -Jenv.type=${props.envType} -JVUSERS=${usersCount} -JHOSTNAME=${props.targetUrl} -JRAMP_UP=${props.rampUp} -JDURATION=${props.duration} -Jinflux.host=${props.reportingInstanceUrl} \"}' \
+                        -e '{\"cmd\": \"-n -t /mnt/jmeter/${testName}.jmx -Jtest_name=${testName} -JDISTRIBUTION=${props.distribution} -Jtenant=${props.tenant} -Jtest.type=${props.testType} -Jenv.type=${props.envType} -JVUSERS=${usersCount} -JHOSTNAME=${props.targetUrl} -JRAMP_UP=${props.rampUp} -JDURATION=${props.duration} -Jinflux.host=${props.influxDbUrl} \"}' \
                         -r 1 -t perfmeter -q ${props.loadGeneratorsCount} -n performance_test_job"
             }
         }
@@ -165,7 +144,7 @@ def installTelegrafAgent(ctx){
         def nodeName = ctx.targetCluster + '-node-' + i;
         sh(script: "aws ecs register-task-definition --cli-input-json file://${WORKSPACE}/carrier-io/scripts/cloudformation/telegraf.json")
         sh(script: """aws ecs start-task --cluster ${ctx.targetCluster} \
-            --overrides '{ "containerOverrides": [ { "name": "busybox", "environment": [ { "name": "INFLUX_HOST", "value": "http://${ctx.reportingInstanceUrl}:8086" }, { "name": "TELEGRAF_NODE_HOSTNAME", "value": "${nodeName}" } ] } ] }' \
+            --overrides '{ "containerOverrides": [ { "name": "busybox", "environment": [ { "name": "INFLUX_HOST", "value": "http://${ctx.influxDbUrl}:8086" }, { "name": "TELEGRAF_NODE_HOSTNAME", "value": "${nodeName}" } ] } ] }' \
             --task-definition telegraf-monitoring --container-instances ${allInstances[i-1]}""")
     }
 }
@@ -203,8 +182,23 @@ def stopAllServices(ctx){
 
 def sendNotification(ctx, testName, usersCount){
     withCredentials([string(credentialsId: 'perf_slack_token_u51', variable: 'slackToken')]) {
-        sh(script: """curl -sSL -X POST -H "Content-Type: application/json" -d '{"notification_type": "api","test": "${testName}", "test_type": "${ctx.testType}", "users": "${usersCount}", "slack_channel": "#ptf_reports","slack_token": "${slackToken}", "influx_host": "${ctx.reportingInstanceUrl}"}' ${ctx.notificationsWebHook}""")
+        sh(script: """curl -sSL -X POST -H "Content-Type: application/json" -d '{"notification_type": "api","test": "${testName}", "test_type": "${ctx.testType}", "users": "${usersCount}", "slack_channel": "#ptf_reports","slack_token": "${slackToken}", "influx_host": "${ctx.influxDbUrl}"}' ${ctx.notificationsWebHook}""")
     }
+}
+
+def getContextSingle() {
+  
+    def ctx = readProperties file: 'carrier-io/system.properties'
+
+    ctx.testName            = params.testName
+    ctx.bucket              = params.bucket
+    ctx.duration            = params.duration
+    ctx.users               = params.users
+    ctx.rampUp              = params.rampUp
+    ctx.loadGeneratorsCount = params.loadGeneratorsCount
+
+    return ctx
+
 }
 
 def getContext() {
