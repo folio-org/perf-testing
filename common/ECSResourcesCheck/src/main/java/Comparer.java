27639808710e9b56714bca507700d9bd0df6cee7
Roman-Fedynyshyn <roman_fedynyshyn@epam.com>
2023-01-18 15:47:16 +0200

    Added changes according to git comments. renamed variables

--- common/ECSResourcesCheck/src/main/java/Comparer.java
+++ common/ECSResourcesCheck/src/main/java/Comparer.java
@@ -1,7 +1,7 @@
 import org.json.JSONObject;
 public class Comparer {
 
-    public void compare(final JSONObject jsonBase, final String cluster, final ECSResources ecs) {
+    public void compare(final JSONObject jsonBase, final String clusterName, final ECSResources ecs) {
         Object[] arr = jsonBase.keySet().toArray();
         JSONObject output = new JSONObject();
         int key = jsonBase.keySet().toArray().length;
@@ -11,55 +11,55 @@ public class Comparer {
             JSONObject base = jsonBase.getJSONObject(module);
             JSONObject target = ecs.describeTaskDef(module);
             if (target != null) {
-                checkDifferences(target, base, cluster);
-                output.accumulate(module, checkDifferences(target, base, cluster));
+                checkDifferences(target, base, clusterName);
+                output.accumulate(module, checkDifferences(target, base, clusterName));
             }
             else {
-                System.out.println("there is no such module in cluster " + cluster);
+                System.out.println("there is no such module in cluster " + clusterName);
                 output.accumulate(module, "no such module");
             }
         }
         System.out.println(output);
     }
-    public JSONObject checkDifferences(final JSONObject target, final JSONObject base, final String cluster) {
+    public JSONObject checkDifferences(final JSONObject target, final JSONObject base, final String clusterName) {
         JSONObject output = new JSONObject();
             if (base.toString().equals(target.toString())) {
                 return null;
             } else {
                 if (base.getInt("SoftLimit") != target.getInt("SoftLimit")) {
-                    output.put("SoftLimit-cluster-" + cluster, target.getInt("SoftLimit"));
+                    output.put("SoftLimit-cluster-" + clusterName, target.getInt("SoftLimit"));
                     output.put("SoftLimit-input", base.getInt("SoftLimit"));
                 }
                 if (base.getInt("XMX") != target.getInt("XMX")) {
-                    output.put("XMX-cluster-" + cluster, target.getInt("XMX"));
+                    output.put("XMX-cluster-" + clusterName, target.getInt("XMX"));
                     output.put("XMX-input", base.getInt("XMX"));
                 }
                 if (!base.getString("Version").equals(target.getString("Version"))) {
-                    output.put("Version-cluster-" + cluster, target.getString("Version"));
+                    output.put("Version-cluster-" + clusterName, target.getString("Version"));
                     output.put("Version-input", base.getString("Version"));
                 }
                 if (base.getInt("desiredCount") != target.getInt("desiredCount")) {
-                    output.put("desiredCount-cluster-" + cluster, target.getInt("desiredCount"));
+                    output.put("desiredCount-cluster-" + clusterName, target.getInt("desiredCount"));
                     output.put("desiredCount-input", base.getInt("desiredCount"));
                 }
                 if (base.getInt("CPUUnits") != target.getInt("CPUUnits")) {
-                    output.put("CPUUnits-cluster-" + cluster, target.getInt("CPUUnits"));
+                    output.put("CPUUnits-cluster-" + clusterName, target.getInt("CPUUnits"));
                     output.put("CPUUnits-input", base.getInt("CPUUnits"));
                 }
                 if (base.getBoolean("RWSplitEnabled") != target.getBoolean("RWSplitEnabled")) {
-                    output.put("RWSplitEnabled-cluster-" + cluster, target.getBoolean("RWSplitEnabled"));
+                    output.put("RWSplitEnabled-cluster-" + clusterName, target.getBoolean("RWSplitEnabled"));
                     output.put("RWSplitEnabled-input", base.getBoolean("RWSplitEnabled"));
                 }
                 if (base.getInt("HardLimit") != target.getInt("HardLimit")) {
-                    output.put("HardLimit-cluster-" + cluster, target.getInt("HardLimit"));
+                    output.put("HardLimit-cluster-" + clusterName, target.getInt("HardLimit"));
                     output.put("HardLimit-input", base.getInt("HardLimit"));
                 }
                 if (base.getInt("Metaspace") != target.getInt("Metaspace")) {
-                    output.put("Metaspace-cluster-" + cluster, target.getInt("Metaspace"));
+                    output.put("Metaspace-cluster-" + clusterName, target.getInt("Metaspace"));
                     output.put("Metaspace-input", base.getInt("Metaspace"));
                 }
                 if (base.getInt("MaxMetaspaceSize") != target.getInt("MaxMetaspaceSize")) {
-                    output.put("MaxMetaspaceSize-cluster-" + cluster, target.getInt("MaxMetaspaceSize"));
+                    output.put("MaxMetaspaceSize-cluster-" + clusterName, target.getInt("MaxMetaspaceSize"));
                     output.put("MaxMetaspaceSize-input", base.getInt("MaxMetaspaceSize"));
                 }
             }
