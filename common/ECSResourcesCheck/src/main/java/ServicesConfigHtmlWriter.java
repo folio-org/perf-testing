cdf0efb355bd6a5aceb2efc0610722929950d3ee
“Roman <roman_fedynyshyn@epam.com>
2024-07-09 11:03:27 +0300

    added changes to pom.xml

--- common/ECSResourcesCheck/src/main/java/ServicesConfigHtmlWriter.java
+++ common/ECSResourcesCheck/src/main/java/ServicesConfigHtmlWriter.java
@@ -1,6 +1,6 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,7 +16,6 @@ public class ServicesConfigHtmlWriter {
     private String generateHtml(JSONObject json, Date date, String cluster) {
         StringBuilder sb = new StringBuilder();
 
-        // Head section with CSS styles
         sb.append("<html>\n<head>\n<style type=\"text/css\">\n");
         sb.append(".tg  {border-collapse:collapse;border-spacing:0;margin:auto;}\n");
         sb.append(".tg td{border-color:#ccc;border-style:solid;border-width:1px;color:#333;font-family:Arial, sans-serif;font-size:14px;padding:12px 8px;word-break:normal;}\n");
@@ -24,7 +23,6 @@ public class ServicesConfigHtmlWriter {
         sb.append(".tg .tg-73oq{text-align:left;vertical-align:top}\n");
         sb.append("</style>\n</head>\n");
 
-        // Body section with table and content
         sb.append("<body style=\"font-family: Arial, sans-serif;\">\n");
         sb.append("<h2 style=\"text-align: center;\">Cluster Resources - ").append(cluster).append(" (").append(date).append(")</h2>\n");
         sb.append("<table class=\"tg\">\n<thead>\n<tr>\n");
@@ -41,7 +39,6 @@ public class ServicesConfigHtmlWriter {
         sb.append("<th class=\"tg-73oq\">R/W Split Enabled</th>\n");
         sb.append("</tr>\n</thead>\n<tbody>\n");
 
-        // Iterate over JSON data and generate table rows
         for (String moduleName : json.keySet()) {
             JSONObject moduleData = json.getJSONObject(moduleName);
             sb.append("<tr>\n");
@@ -57,11 +54,30 @@ public class ServicesConfigHtmlWriter {
             sb.append("<td class=\"tg-73oq\">").append(moduleData.getInt("MaxMetaspaceSize")).append("</td>\n");
             sb.append("<td class=\"tg-73oq\">").append(moduleData.getBoolean("RWSplitEnabled")).append("</td>\n");
             sb.append("</tr>\n");
+
+            // Add sidecars
+            if (moduleData.has("sidecars")) {
+                JSONArray sidecars = moduleData.getJSONArray("sidecars");
+                for (int i = 0; i < sidecars.length(); i++) {
+                    JSONObject sidecar = sidecars.getJSONObject(i);
+                    sb.append("<tr>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(moduleName).append(" - Sidecar ").append(i + 1).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append("N/A").append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getString("Version")).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append("N/A").append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getInt("HardLimit")).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getInt("SoftLimit")).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getInt("CPUUnits")).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getInt("XMX")).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getInt("Metaspace")).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getInt("MaxMetaspaceSize")).append("</td>\n");
+                    sb.append("<td class=\"tg-73oq\">").append(sidecar.getBoolean("RWSplitEnabled")).append("</td>\n");
+                    sb.append("</tr>\n");
+                }
+            }
         }
 
-        // End of table and body
         sb.append("</tbody>\n</table>\n</body>\n</html>");
-
         return sb.toString();
     }
 
